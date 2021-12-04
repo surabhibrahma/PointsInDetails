31873f4314a45d389feaafb0fcff8e7a01955b88
surabhibrahma <contact.surabhibrahma@gmail.com>
2021-12-04 19:16:01 +0530

    Adding logic for refresh token

--- points-in-details-backend/src/dbquery.service.ts
+++ points-in-details-backend/src/dbquery.service.ts
@@ -2,6 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 export class Question {
     ID: number;
@@ -13,12 +14,22 @@ export class Question {
 
 @Injectable()
 export class DBQueryService {
-    constructor(private httpService: HttpService){}
+    refreshToken: string;
+    constructor(private httpService: HttpService, private authService: AuthService){
+        this.authService.getAuth().subscribe(
+            res =>{
+                this.refreshToken = res.data['refreshToken'];
+            }
+        );
+    }
 
     //add database URL
     private databaseURL:string = '';
 
     getAllFromDB(): Observable<AxiosResponse<Question[]>>{
+        const reqHeaders = {
+            'auth': this.refreshToken
+        }
         return this.httpService.get(this.databaseURL+'.json').pipe(res => {
             return res;
         });
